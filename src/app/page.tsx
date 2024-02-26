ae6d1fb7be46434fd32fd18eb3102ef33b1439b2
enslimeseth <ens.limes.eth@gmail.com>
2024-02-26 08:27:33 -0700

    Added Styling to Homepage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,8 +2,13 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <div>
-      <h1>Onchain Clarity</h1>
+    <div style={{ paddingTop: '40px', paddingLeft: '40px', paddingRight: '40px' }}>
+      <h1 style={{ fontSize: '30px', lineHeight: '1.2' }}> 
+        <span style={{ display: 'block', whiteSpace: 'nowrap', marginBottom: '-10px' }}>Onchain</span>
+        <span style={{ display: 'block', whiteSpace: 'nowrap', marginBottom: '-10px' }}>Clarity</span> 
+        <span style={{ display: 'block', whiteSpace: 'nowrap' }}>Co.</span>
+      </h1>
+      <hr style={{ border: '0.5px solid black', margin: '10 auto', width: '100%' }} />
     </div>
   );
 }
