6519f4389c3791473ab340384b1c26d82c83845a
ncale <nicholasbrodeur@outlook.com>
2024-02-29 16:33:17 -0700

    expand airstack query to fetch base action hash, and fix bug in retrieving the latest tx

--- src/airstack.types.ts
+++ src/airstack.types.ts
@@ -37,5 +37,6 @@ interface Image {
 }
 
 interface TokenTransfer {
-    blockTimestamp: string | null
+    blockTimestamp: string | undefined
+    transactionHash: string | undefined
 }
--- src/pages/api/friends.ts
+++ src/pages/api/friends.ts
@@ -33,8 +33,9 @@ export async function fetchAirstackData(fid: number) {
               profileDisplayName
               userId
             }
-            tokenTransfers(input: {blockchain: base, order: [{blockTimestamp: DESC}], limit: 1}) {
+            tokenTransfers(input: {blockchain: base, order: [{blockTimestamp: DESC}]}) {
               blockTimestamp
+              transactionHash
             }
           }
         }
@@ -55,14 +56,30 @@ export async function fetchAirstackData(fid: number) {
   const airstackRes = await res.json() as AirstackResponse;
 
   const formattedData: FormattedAirstackData[] = airstackRes.data.SocialFollowings.Following.map((user) => {
-    const latestBaseActionTimestamp = user.followingAddress.tokenTransfers[0]?.blockTimestamp
+    
+    let latestBaseAction: {
+      blockTimestamp: string | undefined
+      transactionHash: string | undefined
+    } = { blockTimestamp: undefined, transactionHash: undefined }
+
+    if (user.followingAddress.tokenTransfers.length > 0) {
+      const baseTxns = user.followingAddress.tokenTransfers
+      const maxTimeObj = baseTxns.reduce((maxObj, txnObj) => {
+        if (txnObj.blockTimestamp == undefined || txnObj.blockTimestamp == undefined || maxObj.blockTimestamp == undefined) return maxObj
+        return Date.parse(txnObj.blockTimestamp) > Date.parse(maxObj.blockTimestamp) ? txnObj : maxObj
+      }, baseTxns[0])
+      latestBaseAction.blockTimestamp = maxTimeObj.blockTimestamp
+      latestBaseAction.transactionHash = maxTimeObj.transactionHash
+    }
+    
     return {
       displayName: user.followingAddress.socials[0].profileDisplayName,
       username: user.followingAddress.socials[0].profileName,
       fid: user.followingAddress.socials[0].userId,
       avatar: user.followingAddress.socials[0].profileImageContentValue.image?.medium,
       latestFarcasterAction: new Date(user.followingAddress.socials[0].updatedAt),
-      latestBaseAction: latestBaseActionTimestamp ? new Date(latestBaseActionTimestamp) : null,
+      latestBaseAction: latestBaseAction.blockTimestamp ? new Date(latestBaseAction.blockTimestamp) : undefined,
+      latestBaseActionHash: latestBaseAction.transactionHash ? latestBaseAction.transactionHash : undefined,
     }
   })
 
@@ -75,5 +92,6 @@ export type FormattedAirstackData = {
   fid: string
   avatar: string | undefined
   latestFarcasterAction: Date
-  latestBaseAction: Date | null
+  latestBaseAction: Date | undefined
+  latestBaseActionHash: string | undefined
 }
