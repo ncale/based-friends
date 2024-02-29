09a8e72563541932438d63e2572d87fae1a78c6b
ncale <nicholasbrodeur@outlook.com>
2024-02-28 18:05:43 -0700

    update footer styles

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,7 +1,9 @@
 export default async function Footer() {
-    return (
-                    <footer className="" style={{position: 'absolute', bottom: '20px', width: '100%', textAlign: 'right', paddingRight: '150px'}}>
-            <small style={{color: '#FFF'}}>Made by Onchain Clarity Co.</small>
-        </footer>
-                );
+	return (
+		<footer className="flex justify-end px-20 w-full">
+			<small className="text-white">Made by Onchain Clarity Co.</small>
+		</footer>
+	);
 }
+
+// style={{position: 'absolute', bottom: '20px', width: '100%', textAlign: 'right', paddingRight: '150px'}}
