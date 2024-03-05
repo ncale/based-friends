9bcdbc0ad7aee08bbae7155f189d904e4a63bc69
ncale <nicholasbrodeur@outlook.com>
2024-03-05 11:56:15 -0700

    center log in button for mobile

--- src/components/LogInPage.tsx
+++ src/components/LogInPage.tsx
@@ -4,7 +4,7 @@ export default function LogInPage() {
     return (
         <>
             <p className="text-white justify-center hidden md:flex">Please log in</p>
-            <span className="flex justify-center mt-2 md:hidden"><AuthButton/></span>
+            <span className="flex items-center justify-center mt-2 md:hidden" style={{height: "60vh"}}><AuthButton/></span>
         </>
     )
 }
