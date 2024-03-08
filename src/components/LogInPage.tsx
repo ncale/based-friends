74238d0b14749e73c379ddaaa994c75459b47c98
ncale <nicholasbrodeur@outlook.com>
2024-03-08 08:49:58 -0700

    fix login page not showing on desktop

--- src/components/LogInPage.tsx
+++ src/components/LogInPage.tsx
@@ -1,10 +1,18 @@
 import AuthButton from "./AuthButton"
 
 export default function LogInPage() {
-    return (
-        <>
-            <p className="text-white justify-center hidden md:flex">Please log in</p>
-            <span className="flex items-center justify-center mt-2 md:hidden" style={{height: "60vh"}}><AuthButton/></span>
-        </>
-    )
+	return (
+		<>
+			{/* desktop view */}
+			<main className="hidden md:flex md:flex-col md:items-center mt-20 text-white">
+				<p>DESKTOP VIEW</p>
+				<p>Please log in</p>
+			</main>
+			{/* mobile view */}
+			<main className="flex flex-col items-center justify-center mt-2 md:hidden text-white" style={{height: "60vh"}}>
+				<p>Welcome to BasedFriends!</p>
+				<AuthButton/>
+			</main>
+		</>
+	)
 }
