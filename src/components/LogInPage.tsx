18b35883fe265b41f8d36c369193f0885cb4cf4b
limes <140857816+enslimeseth@users.noreply.github.com>
2024-03-19 11:03:35 +0000

    Mobile update (#20)

--- src/components/LogInPage.tsx
+++ src/components/LogInPage.tsx
@@ -5,35 +5,51 @@ export default function LogInPage() {
     <>
       {/* desktop view */}
       <main className="hidden md:flex md:flex-col md:items-center mt-32 text-white">
-        <p className="mb-4"> </p>
-        <p className="mb-4">Welcome to BasedFriends!</p>
-        <p className="mb-4">
+        <p className="mb-4 text-5xl">Welcome to BasedFriends!</p>
+        <p className="mb-4 max-w-lg text-center">
           BasedFriends is a friends list based on your Farcaster social graph
-          that tells you who is online and who is onchain.
+          that tells you who is <span className="underline">online</span> and
+          who is <span className="underline">onchain</span>.
         </p>
-        <div className="flex flex-col items-center">
-          <div className="flex justify-center">
-            <p>
-              <span className="underline">Online</span>: Cast, Like,
-              or Recast on Farcaster within the last 10 mins
-            </p>
-          </div>
-          <div className="flex justify-center">
-            <p className="mb-4">
-              <span className="underline">Onchain</span>: Transaction on the
-              Base Blockchain within the last 24 hours
-            </p>
-          </div>
+        <div className="grid gap-y-1 border p-4 mb-4">
+          <p>
+            <span className="underline">Online</span>: Cast, Like, or Recast on
+            Farcaster within the last 10 mins
+          </p>
+          <p>
+            <span className="underline">Onchain</span>: Transaction on the Base
+            Blockchain within the last 24 hours
+          </p>
         </div>
+        
         <p>Simply Sign In with Farcaster in the top right to get started. </p>
       </main>
       {/* mobile view */}
       <main
         className="flex flex-col items-center justify-center mt-2 md:hidden text-white"
-        style={{ height: "60vh" }}
-      >
-        <p>Welcome to BasedFriends!</p>
-        <AuthButton />
+>
+        <p className="mb-20"> </p>
+        <p className="mb-4 max-w-lg text-center">
+          BasedFriends is a friends list based on your Farcaster social graph
+          that tells you who is <span className="underline">online</span> and
+          who is <span className="underline">onchain</span>.
+        </p>
+        <div className="grid gap-y-1 border p-4 mb-4">
+          <p>
+            <span className="underline">Online</span>: Cast, Like, or Recast on
+            Farcaster within the last 10 mins
+          </p>
+          <p>
+            <span className="underline">Onchain</span>: Transaction on the Base
+            Blockchain within the last 24 hours
+          </p>
+        </div>
+        <p className="mb-4">
+          Connect with Farcaster to get started
+        </p>
+        <div>
+          <AuthButton />
+        </div>
       </main>
     </>
   );
